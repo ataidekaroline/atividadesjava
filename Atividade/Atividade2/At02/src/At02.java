e6da8a31332c29a8bc2ee250bab202609cb29d19
Luana Ataide <72031716+ataidekaroline@users.noreply.github.com>
2024-11-15 01:00:21 -0300

    Add files via upload

---
+++ Atividade/Atividade2/At02/src/At02.java
@@ -0,0 +1,11 @@
+public class At02 {
+    public static void main(String[] args) throws Exception {
+    /*faça um programa para ler dois valores inteiros, e depois mostrar
+    * na tela a soma desses números com uma mensagem explicativa
+    */
+    int num1 = 10;
+    int num2 = 30;
+    
+    System.out.println("A soma dos valores " + num1 + " e " + num2 + " é: " + (num1 + num2));
+    }
+}
